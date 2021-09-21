fb691d07bb74191b26bb5ecb06ae58fd5757ebf0
kyshine <kyshine@163.com>
2021-09-21 11:32:51 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @Author shine
+ * @Date 2021/9/21 11:28
+ * @Version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
